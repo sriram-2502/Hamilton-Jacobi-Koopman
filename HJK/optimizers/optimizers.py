04c768864d7042508f65558435fc15d419f5d418
sriram-2502 <sriramk@clemson.edu>
2022-05-22 21:35:50 -0400

    issues using casadi

--- HJK/optimizers/optimizers.py
+++ HJK/optimizers/optimizers.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cvxpy as cp
+from casadi import *
+
 
 def cvx (dim, Nbs, A, X, sys, params, Psi , DPsi):
 
@@ -39,3 +41,44 @@ def cvx (dim, Nbs, A, X, sys, params, Psi , DPsi):
     print("optimal objective value: {}".format(objective.value))
     print(U.value)
     return U.value
+
+
+def casadi(dim, Nbs, A, X, sys, params, Psi , DPsi):
+
+    # get xdot values
+    t = 0
+    u = 0
+    F_z = sys(0,X,0,params)
+
+    # build gradient matrix grad_Psi * f(x)
+    Df_z = []
+    numIC = X.shape[1]
+    for i in range(numIC):
+        Df_z.append(DPsi[:,:,i] @ F_z[:,i])
+    Df_z = np.array(Df_z).T
+
+    # get linear part
+    E = A
+    Ez = E @ X
+
+    # get observables and size
+    G_z = Psi
+    # get length of Psi to determine coefficient U dimensions
+    Nbs = np.shape(G_z)[0]
+
+
+    U = DM(dim, Nbs)
+    U_sparse = U.sparse()
+    f = U @ Df_z + F_z - A @ U @ G_z - Ez
+
+    nlp = {}                # NLP declaration
+    nlp['x'] = U_sparse     # decision vars
+    nlp['f'] = f            # objective
+
+    # Create solver instance
+    F = nlpsol('F','ipopt',nlp);
+
+    # Solve the problem using a guess
+    F(x0=[2.5,3.0],ubg=0,lbg=0)
+
+    return F
